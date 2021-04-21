806bbaaaa946e8b330ef2310e6619046ea297cb8
Alexander Tack <alexander.tack@student.vives.be>
2021-04-21 19:00:20 +0200

    Add sample data and try out schemas

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// server
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const {
+    GraphQLSchema,
+    GraphQLObjectType,
+    GraphQLString
+} = require('graphql');
+
+const app = express();
+
+const checkouts = [
+    { id: 1, name: "first checkout", score: 40 },
+    { id: 2, name: "first checkout", score: 40 },
+    { id: 3, name: "first checkout", score: 40 },
+    { id: 4, name: "first checkout", score: 40 },
+    { id: 5, name: "first checkout", score: 40 }
+];
+
+const schema = new GraphQLSchema({
+    query: new GraphQLObjectType({
+        name: "Test",
+        fields: () => ({
+            message: {
+                type: GraphQLString,
+                resolve: () => "Hello world"
+            }
+        })
+    })
+})
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    graphiql: true
+}));
+
+const port = 5000;
+app.listen(port, () => console.log(`Listening on ${port}`));
